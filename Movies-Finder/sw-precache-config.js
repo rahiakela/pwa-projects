62ededbf422c24e45038d88393a681885ec16583
rahiakela <rahi.akela@yahoo.com>
2018-07-31 09:22:23 +0530

    Caching is done

--- Movies-Finder/sw-precache-config.js
+++ Movies-Finder/sw-precache-config.js
@@ -3,4 +3,42 @@ module.exports = {
   stripPrefix: 'dist',
   root: 'dist',
   staticFileGlobs: ['dist/index.html', 'dist/**.js', 'dist/**.css'],
+  runtimeCaching: [
+    {
+      urlPattern: '/*',
+      handler: 'cacheFirst',
+      options: {
+        origin: 'tmdb.org',
+        cache: {
+          maxEntries: 100,
+          name: 'movie-cache',
+        },
+      },
+    },
+    {
+      urlPattern: '/*',
+      handler: 'cacheFirst',
+      options: {
+        origin: 'themoviedb.org',
+        cache: {
+          maxEntries: 10,
+          name: 'movie-cache',
+        },
+      },
+    },
+    {
+      urlPattern: '/*',
+      handler: 'cacheFirst',
+      options: {
+        origin: 'bootstrapcdn.com',
+      },
+    },
+    {
+      urlPattern: '/*',
+      handler: 'cacheFirst',
+      options: {
+        origin: 'jquery.com',
+      },
+    },
+  ],
 }
