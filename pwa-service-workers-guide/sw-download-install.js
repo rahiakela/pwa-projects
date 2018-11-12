edb1871bcb544dcc62c724de090ef038a6028128
rahiakela <rahi.akela@yahoo.com>
2018-11-12 10:17:14 +0530

    Setup is done

--- pwa-service-workers-guide/sw-download-install.js
+++ pwa-service-workers-guide/sw-download-install.js
@@ -1,86 +1,64 @@
-
-
-
-const VERSION = 'v3';
-
+const VERSION = 'v4';
 
 self.addEventListener('install', event => event.waitUntil(installServiceWorker()));
 
-
 async function installServiceWorker() {
+  log('Service Worker installation started ');
 
-    log("Service Worker installation started ");
+  const cache = await caches.open(getCacheName());
 
-    const cache = await caches.open(getCacheName());
-
-    return cache.addAll([
-        '/',
-        'carousel.css',
-        'http://getbootstrap.com/dist/css/bootstrap.min.css',
-        'https://code.jquery.com/jquery-3.2.1.slim.min.js',
-        'http://getbootstrap.com/assets/js/vendor/popper.min.js',
-        'http://getbootstrap.com/dist/js/bootstrap.min.js',
-        'http://getbootstrap.com/assets/js/vendor/holder.min.js'
-    ]);
+  return cache.addAll([
+    '/',
+    'carousel.css',
+    'https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css',
+    'https://code.jquery.com/jquery-3.3.1.slim.min.js',
+    'https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js',
+    'https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js',
+    'https://cdnjs.cloudflare.com/ajax/libs/holder/2.9.6/holder.min.js',
+  ]);
 }
 
 self.addEventListener('activate', () => activateSW());
 
-
 async function activateSW() {
+  log('Service Worker activated');
 
-    log('Service Worker activated');
+  const cacheKeys = await caches.keys();
 
-    const cacheKeys = await caches.keys();
-
-    cacheKeys.forEach(cacheKey => {
-        if (cacheKey !== getCacheName() ) {
-            caches.delete(cacheKey);
-        }
-    });
-
+  cacheKeys.forEach(cacheKey => {
+    if (cacheKey !== getCacheName()) {
+      caches.delete(cacheKey);
+    }
+  });
 }
-
 
 self.addEventListener('fetch', event => event.respondWith(cacheThenNetwork(event)));
 
-
-
 async function cacheThenNetwork(event) {
+  const cache = await caches.open(getCacheName());
 
-    const cache = await caches.open(getCacheName());
+  const cachedResponse = await cache.match(event.request);
 
-    const cachedResponse = await cache.match(event.request);
+  if (cachedResponse) {
+    log('Serving From Cache: ' + event.request.url);
+    return cachedResponse;
+  }
 
-    if (cachedResponse) {
-        log('Serving From Cache: ' + event.request.url);
-        return cachedResponse;
-    }
+  const networkResponse = await fetch(event.request);
 
-    const networkResponse = await fetch(event.request);
+  log('Calling network: ' + event.request.url);
 
-    log('Calling network: ' + event.request.url);
-
-    return networkResponse;
-
-
+  return networkResponse;
 }
-
-
-
-
-
 
 function getCacheName() {
-    return "app-cache-" + VERSION;
+  return 'app-cache-' + VERSION;
 }
 
-
 function log(message, ...data) {
-    if (data.length > 0) {
-        console.log(VERSION, message, data);
-    }
-    else {
-        console.log(VERSION, message);
-    }
+  if (data.length > 0) {
+    console.log(VERSION, message, data);
+  } else {
+    console.log(VERSION, message);
+  }
 }
--- pwa-service-workers-guide/sw-register.js
+++ pwa-service-workers-guide/sw-register.js
@@ -1,21 +1,11 @@
 if ('serviceWorker' in navigator) {
-
-    window.addEventListener('load', function () {
-
-
-        navigator.serviceWorker.register('/sw-download-install.js', {
-            scope: '/'
-        })
-            .then(registration => {
-
-                console.log("Service Worker registration completed ...");
-
-            });
-
-    });
-
-
-
-
+  window.addEventListener('load', function() {
+    navigator.serviceWorker
+      .register('/sw-download-install.js', {
+        scope: '/',
+      })
+      .then(registration => {
+        console.log('Service Worker registration completed ...');
+      });
+  });
 }
-
