366d1db2ae605089199a5deeb9b4caa04a99e312
rahiakela <rahi.akela@yahoo.com>
2018-11-23 10:45:20 +0530

    App update notification is done

---
+++ pwa-angular-university-course/src/app/app.component.ts
@@ -0,0 +1,25 @@
+import {Component, OnInit} from '@angular/core';
+import {SwUpdate} from '@angular/service-worker';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent implements OnInit{
+  title = 'pwa-angular-university-course';
+
+  constructor(private swUpdate: SwUpdate) {
+
+  }
+
+  ngOnInit(): void {
+    if (this.swUpdate.isEnabled) {
+      this.swUpdate.available.subscribe(() => {
+        if (confirm('New version available. Load New Version?')) {
+          window.location.reload();
+        }
+      });
+    }
+  }
+}
